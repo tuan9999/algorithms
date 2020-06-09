dd8d1402f94d32f8f0e58bb39a6f9ff016454aa3
tuan9999 <tuanperera@gmail.com>
2020-06-09 15:02:56 +0200

    U

---
+++ merge_sort.js
@@ -0,0 +1,57 @@
+// Implementation of recursive merge sort
+
+// Takes in an array that has two sorted subarrays,
+//  from [p..q] and [q+1..r], and merges the array
+var merge = function(array, p, q, r) {
+    var lowHalf = [];
+    var highHalf = [];
+
+    var k = p;
+    var i;
+    var j;
+    for (i = 0; k <= q; i++, k++) {
+        lowHalf[i] = array[k];
+    }
+    for (j = 0; k <= r; j++, k++) {
+        highHalf[j] = array[k];
+    }
+
+    k = p;
+    i = 0;
+    j = 0;
+
+    // Repeatedly compare the lowest untaken element in
+    //  lowHalf with the lowest untaken element in highHalf
+    //  and copy the lower of the two back into array
+    while (i < lowHalf.length && j < highHalf.length) {
+        if (lowHalf[i] < highHalf[j]) {
+            array[k] = lowHalf[i];
+            i++;
+        } else {
+            array[k] = highHalf[j];
+            j++;
+        }
+        k++;
+    }
+    
+    // Once one of lowHalf and highHalf has been fully copied
+    //  back into array, copy the remaining elements from the
+    //  other temporary array back into the array
+    while (i < lowHalf.length) {
+        array[k++] = lowHalf[i++];
+    }
+    while (i < highHalf.length) {
+        array[k++] = highHalf[j++];
+    }
+};
+
+// Takes in an array and recursively merge sorts it
+var mergeSort = function(array, p, r) {
+    if (r > p) {
+        var q = Math.floor((p + r) / 2);
+        mergeSort(array, p, q);
+        mergeSort(array, q + 1, r);
+        merge(array, p, q, r);
+    }
+    return array;
+};
